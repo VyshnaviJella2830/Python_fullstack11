45022b6d3197c3cd9ec42fc1bb1d690a740fb4c7
VyshnaviJella2830 <jellavyshnavi33@gmail.com>
2026-08-04 10:48:30 +0000

    upto form method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 
 app = Flask(__name__)
 
@@ -24,7 +24,16 @@ def courses():
 def trainers():
     return render_template("trainers.html")
 
-
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"]
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
