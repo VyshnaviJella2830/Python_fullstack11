39d3228d9688dac9d875044f61db5aade7545d36
VyshnaviJella2830 <jellavyshnavi33@gmail.com>
2026-08-05 10:43:17 +0000

    css html and app.py updated

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,jsonify,request
 
 app = Flask(__name__)
 
+users_dp = {}
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -35,5 +37,37 @@ def register():
         course=request.form["course"]
     return render_template("register.html")
 
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return render_template("login.html")
+    return render_template("login.html")
+
+@app.route('/api/register', methods=["POST"])
+def api_register():
+    data = request.get_json()
+    email = data.get("email")
+
+    if email in users_db:
+        return jsonify({"status": "error", "message": "user already exists with this email!"}), 400
+
+    users_db[email] = data
+    return jsonify({"status": "success", "message": "Registration successfully!"})
+
+
+@app.route('/api/login', methods=["POST"])
+def api_login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    user = users_db.get(email)
+    if user and user.get("password") == password:
+        return jsonify({"status": "success", "message": "Login successful! Welcome back."})
+    else:
+        return jsonify({"status": "error", "message": "Invalid email or password!"}), 401
+
+        
 if __name__ == "__main__":
     app.run(debug=True)
