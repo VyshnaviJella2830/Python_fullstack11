491d81ef87a065aa9b9721c258ee1e4138f142ef
VyshnaviJella2830 <jellavyshnavi33@gmail.com>
2026-08-11 10:50:26 +0000

    eventlistners

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from flask import Flask,render_template,jsonify,request
+import sqlite3
+
+
+from flask import Flask,render_template,jsonify,request,redirect,url_for
 
 app = Flask(__name__)
+app.secret_key="super_secret_key"
 
-users_dp = {}
+def get_db_connection():
+    conn = sqlite3.connect("users.db")
+    conn.row_factory = sqlite3.row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY
+        AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL,
+        dob TEXT NOT NULL,
+        gender TEXT NOT NULL,
+        course TEXT NOT NULL
+        ))
 
 @app.route('/')
 def home():
@@ -26,16 +49,7 @@ def courses():
 def trainers():
     return render_template("trainers.html")
 
-@app.route('/register', methods=["POST", "GET"])
-def register():
-    if request.method == "POST":
-        name=request.form["name"]
-        email=request.form["email"]
-        password=request.form["password"]
-        dob=request.form["dob"]
-        gender=request.form["gender"]
-        course=request.form["course"]
-    return render_template("register.html")
+
 
 
 @app.route('/login', methods=["POST", "GET"])
@@ -48,6 +62,23 @@ def login():
 def api_register():
     data = request.get_json()
     email = data.get("email")
+    conn = conn.cursor()
+    cursor.execute("SELECT * FROM users
+    WHERE email = ?" (email,))
+    user = cursor.fetchone()
+    if user:
+        return jsonify({"status": "error",
+        "message": "user already exists
+        with this email!"}), 400
+
+    cursor.execute("INSERT INTO users
+    (name, email, password, dob, gender,
+    course) VALUES (?,?,?,?,?,?)",(data["name"], 
+    data["email"], data["password"], data["dob"], data["gender"],
+    data["course"]))
+    conn.commit()
+    conn.close()
+    return jsonify(["status": "success"])
 
     if email in users_db:
         return jsonify({"status": "error", "message": "user already exists with this email!"}), 400
